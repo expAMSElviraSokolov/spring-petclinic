3e9d042458f08c740f6d94128afcfeac362f3794
Elvira Sokolov <70578683+expAMSElviraSokolov@users.noreply.github.com>
2020-12-03 15:17:41 +0100

    takes out curly brace to break build to test ci

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -25,7 +25,7 @@ import javax.validation.constraints.NotEmpty;
  * @author Ken Krebs
  */
 @MappedSuperclass
-public class Person extends BaseEntity {
+public class Person extends BaseEntity 
 
 	@Column(name = "first_name")
 	@NotEmpty
